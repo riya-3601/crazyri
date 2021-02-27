6812e467ff5774378c71cfef16a557083959d63e
sanjanashah9 <shahsanjana139@gmail.com>
2021-02-27 12:51:00 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
